da34e69301ca08eccd689b8d56b8e3b7ae2a1edd
Matheus Oliveira <matheussilvaoliveira@hotmail.com>
2025-04-24 08:22:29 -0400

    finished quiz project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
   <div>
-    <h1>
-      Microphones can be used not only to pick up sound, but also to project
-      sound similar to a speaker.
-    </h1>
+    <ScoreBoard :winCount="win_count" :loseCount="lose_count" />
 
-    <input type="radio" name="options" value="True" />
-    <label>True</label><br />
+    <h1 v-if="question" v-html="question"></h1>
 
-    <input type="radio" name="options" value="False" />
-    <label>False</label><br />
+    <template v-for="answer in answers" :key="answer">
+      <input
+        :disabled="answerSubmitted"
+        type="radio"
+        name="options"
+        :value="answer"
+        v-model="chosen_answer"
+      />
+      <label v-html="answer"></label><br />
+    </template>
 
-    <button class="send" type="button">Send</button>
+    <button class="send" type="button" @click="submitAnswer()">Send</button>
+
+    <section class="result" v-if="answerSubmitted">
+      <template v-if="chosen_answer == correctAnswer">
+        <h4>
+          &#9989; Congratulations, the answer "{{ correctAnswer }}" is correct.
+        </h4>
+      </template>
+      <template v-else>
+        <h4>
+          &#10060; Too bad, the answer is wrong. the correct answer is: "{{
+            correctAnswer
+          }}".
+        </h4>
+      </template>
+      <button @click="getNewQuestion()" class="send" type="button">
+        Próxima pergunta
+      </button>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import axios from "axios";
+
+import ScoreBoard from "./components/ScoreBoard.vue";
 
 export default defineComponent({
   name: "App",
-});
 
-// https://opentdb.com/api.php?amount=1&category=18
+  components: {
+    ScoreBoard,
+  },
+
+  data() {
+    return {
+      chosen_answer: undefined as string | undefined,
+      question: undefined as string | undefined,
+      incorrectAnswers: [] as string[],
+      correctAnswer: "" as string,
+      win_count: 0 as number,
+      lose_count: 0 as number,
+      answerSubmitted: false as boolean,
+    };
+  },
+
+  computed: {
+    answers(): string[] {
+      var answers = [...this.incorrectAnswers];
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswer
+      );
+      return answers;
+    },
+  },
+
+  methods: {
+    getNewQuestion(): void {
+      this.chosen_answer = undefined;
+      this.answerSubmitted = false;
+      this.question = undefined;
+
+      axios
+        .get("https://opentdb.com/api.php?amount=1&category=30")
+        .then((response) => {
+          this.question = response.data.results[0].question;
+          this.incorrectAnswers = response.data.results[0].incorrect_answers;
+          this.correctAnswer = response.data.results[0].correct_answer;
+        });
+    },
+
+    submitAnswer(): void {
+      if (!this.chosen_answer) {
+        alert("Pick one answer!");
+      } else {
+        this.answerSubmitted = true;
+        if (this.chosen_answer === this.correctAnswer) {
+          this.win_count++;
+        } else {
+          this.lose_count++;
+        }
+      }
+    },
+  },
+
+  created() {
+    this.getNewQuestion();
+  },
+});
 </script>
 
 <style lang="scss">
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="score">
+    Player <span>{{ winCount }}</span> x <span>{{ loseCount }}</span> Computer
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["winCount", "loseCount"],
+};
+</script>
+
+<style lang="scss">
+section.score {
+  border-bottom: 1px solid black;
+  padding: 24px;
+  font-size: 18px;
+}
+
+section.score span {
+  padding: 8px;
+  font-weight: bold;
+  border: 1px solid black;
+}
+</style>
